725eed64a91bc52f0326d128ca87544ae092ce35
eliran17e <eliran17eli@gmail.com>
2025-04-08 19:18:29 +0300

    king h file added

---
+++ Chess/include/King.h
@@ -0,0 +1,23 @@
+//
+// Created by User on 05/04/2025.
+//
+
+#ifndef CHESS_KING_H
+#define CHESS_KING_H
+#include "Piece.h"
+
+class King: public Piece {
+public:
+    King(bool isWhite, char symbol, int col, int row);
+    /**
+     *  simple function that just checks king movement as if he was alone on the board
+     * @param toRow
+     * @param toCol
+     * @return if the move is valid
+     */
+    bool isValidMove( int toRow, int toCol) override;
+
+};
+
+
+#endif //CHESS_KING_H
