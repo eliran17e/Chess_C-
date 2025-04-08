d4194b5a59f0f9bd43f4faa03c9d4d63eaab41bb
eliran17e <eliran17eli@gmail.com>
2025-04-08 19:18:40 +0300

    knight h file added

---
+++ Chess/include/Knight.h
@@ -0,0 +1,23 @@
+//
+// Created by User on 05/04/2025.
+//
+
+#ifndef CHESS_KNIGHT_H
+#define CHESS_KNIGHT_H
+#include "Piece.h"
+
+class Knight: public Piece {
+public:
+    Knight(bool isWhite, char symbol, int col, int row);
+    /**
+     *  function checks the knight movement as if he was alone in the board
+     * @param toRow
+     * @param toCol
+     * @return if the move is valid
+     */
+    bool isValidMove( int toRow, int toCol) override;
+
+};
+
+
+#endif //CHESS_KNIGHT_H
