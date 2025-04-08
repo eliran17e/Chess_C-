b3207d839c8003e3b02ac646cbae4b7610835293
eliran17e <eliran17eli@gmail.com>
2025-04-08 19:18:59 +0300

    pawn h file added

---
+++ Chess/include/Pawn.h
@@ -0,0 +1,22 @@
+//
+// Created by User on 05/04/2025.
+//
+
+#ifndef CHESS_PAWN_H
+#define CHESS_PAWN_H
+#include "Piece.h"
+
+class Pawn: public Piece {
+public:
+    Pawn(bool isWhite, char symbol, int col, int row);
+    /**
+     *  check the pawn movement as if he was alone in the board
+     * @param toRow
+     * @param toCol
+     * @return if the move is valid
+     */
+    bool isValidMove( int toRow, int toCol) override;
+};
+
+
+#endif //CHESS_PAWN_H
