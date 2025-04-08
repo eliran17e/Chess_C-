2c7dc9502295c643454ecc67d6a54900e784a9a9
eliran17e <eliran17eli@gmail.com>
2025-04-08 19:29:33 +0300

    main has been modified

--- Chess/src/main.cpp
+++ Chess/src/main.cpp
@@ -1,38 +1,41 @@
 // Chess 
-#include "Chess.h"
+#include "../include/Chess.h"
+#include "../include/Board.h"
 
 int main()
 {
-	string board = "RNBQKBNRPPPPPPPP################################pppppppprnbqkbnr"; 
-//	string board = "##########K###############################R#############r#r#####";
+	string board = "RNBQKBNRPPPPPPPP################################pppppppprnbqkbnr";
+//	string board = "##########K###############################R#############r#rk####";
+    Board b(board);
 	Chess a(board);
 	int codeResponse = 0;
 	string res = a.getInput();
 	while (res != "exit")
 	{
-		/* 
-		codeResponse value : 
-		Illegal movements : 
-		11 - there is not piece at the source  
+		/*
+		codeResponse value :
+		Illegal movements :
+		11 - there is not piece at the source
 		12 - the piece in the source is piece of your opponent
-		13 - there one of your pieces at the destination 
-		21 - illegal movement of that piece 
+		13 - there one of your pieces at the destination
+		21 - illegal movement of that piece
 		31 - this movement will cause you checkmate
 
-		legal movements : 
-		41 - the last movement was legal and cause check 
-		42 - the last movement was legal, next turn 
+		legal movements :
+		41 - the last movement was legal and cause check
+		42 - the last movement was legal, next turn
 		*/
 
-		/**/ 
+		/**/
 		{ // put your code here instead that code
-			cout << "code response >> ";
-			cin >> codeResponse;
+            codeResponse = b.checkMove(res);
+            printf("%d",codeResponse);
+
 		}
 		/**/
 
 		a.setCodeResponse(codeResponse);
-		res = a.getInput(); 
+		res = a.getInput();
 	}
 
 	cout << endl << "Exiting " << endl; 
