b6568b5ecebe00e504ea0d1659f5e62af358237b
eliran17e <eliran17eli@gmail.com>
2025-04-08 19:19:31 +0300

    queen h file added

---
+++ Chess/include/Queen.h
@@ -0,0 +1,23 @@
+//
+// Created by User on 05/04/2025.
+//
+
+#ifndef CHESS_QUEEN_H
+#define CHESS_QUEEN_H
+#include "Piece.h"
+
+class Queen: public Piece{
+public:
+    Queen(bool isWhite, char symbol, int col, int row);
+/**
+ * isValid function checks diagonal and horizontal movement only
+ * @param toRow(location of the queen)
+ * @param toCol (location of the queen)
+ * @return true if the square  it wants to move is horizontal or diagonal to the queen
+ */
+    bool isValidMove( int toRow, int toCol) override;
+
+};
+
+
+#endif //CHESS_QUEEN_H
